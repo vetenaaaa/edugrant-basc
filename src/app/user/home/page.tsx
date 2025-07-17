6793e58841031905356c92b6ae074fd591508017
iamDaedalus <jerometecson.basc@gmail.com>
2025-07-17 20:21:45 +0800

    .

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -1,32 +1,32 @@
-// // "use client";
-// // import { Button } from "@/components/ui/button";
-// // import axios from "axios";
-// // import { useRouter } from "next/navigation";
+"use client";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
-// // export default function HomeClient() {
-// //   const router = useRouter();
-// //   const HandleLogout = async () => {
-// //     try {
-// //       const res = await axios.post(
-// //         `https://edugrant-express-server-production.up.railway.app/user/logout`,
-// //         {},
-// //         { withCredentials: true }
-// //       );
-// //       if (res.status === 200) {
-// //         router.replace("/");
-// //       }
-// //     } catch (error) {
-// //       console.error("Logout failed", error);
-// //       // Still redirect even if logout fails
-// //       router.push("/");
-// //     }
-// //   };
-// //   return (
-// //     <>
-// //       HOME <Button onClick={HandleLogout}>Logout</Button>
-// //     </>
-// //   );
-// // }
+export default function HomeClient() {
+  const router = useRouter();
+  const HandleLogout = async () => {
+    try {
+      const res = await axios.post(
+        `https://edugrant-express-server-production.up.railway.app/user/logout`,
+        {},
+        { withCredentials: true }
+      );
+      if (res.status === 200) {
+        router.replace("/");
+      }
+    } catch (error) {
+      console.error("Logout failed", error);
+      // Still redirect even if logout fails
+      router.push("/");
+    }
+  };
+  return (
+    <>
+      HOME <Button onClick={HandleLogout}>Logout</Button>
+    </>
+  );
+}
 // "use client";
 // import { Button } from "@/components/ui/button";
 // import type React from "react";
