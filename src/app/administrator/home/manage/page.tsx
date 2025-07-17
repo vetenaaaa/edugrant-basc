4a47d151a6426a1a64fa78b72a718e0c6f27c00d
iamDaedalus <jerometecson.basc@gmail.com>
2025-07-18 02:16:02 +0800

    .

--- src/app/administrator/home/manage/page.tsx
+++ src/app/administrator/home/manage/page.tsx
@@ -27,7 +27,6 @@ import { useState } from "react";
 
 import { Input } from "@/components/ui/input";
 import useScholarshipData from "@/lib/scholarship-data";
-import useScholarshipSearch from "@/lib/scholarship-search";
 const headers = [
   { label: "Scholarship" },
   { label: "Provider" },
@@ -36,9 +35,10 @@ const headers = [
 ];
 
 export default function Manage() {
-  const [query, setQuery] = useState("");
   const { data, loading } = useScholarshipData();
-  const { searchData } = useScholarshipSearch();
+  const [intercept, setIntercept] = useState<"scholars" | "details">(
+    "scholars"
+  );
 
   return (
     <div className="pl-1 pr-2 your-class  h-screen">
@@ -74,88 +74,66 @@ export default function Manage() {
           <ModeToggle />
         </div>
       </header>
-      <div className="mx-auto lg:w-3/4 w-[95%] py-10">
-        <h1 className="text-3xl font-semibold">Manage Scholarships</h1>
-        <p className="text-sm text-gray-500 mt-1">
-          Browse the list of active scholarships. Use the available actions to
-          modify or remove entries.
-        </p>
-        <div className="container mx-auto py-10 space-y-3">
-          <div>
-            <Input
-              placeholder="Search "
-              onChange={(e) => setQuery(e.target.value)}
-            />
-          </div>
-          <Table>
-            <TableCaption>A list of active scholarships.</TableCaption>
-            <TableHeader>
-              <TableRow>
-                {headers.map((header) => (
-                  <TableHead key={header.label}>{header.label}</TableHead>
-                ))}
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {loading ? (
+      {intercept === "scholars" && (
+        <div className="mx-auto lg:w-3/4 w-[95%] py-10">
+          <h1 className="text-3xl font-semibold">Manage Scholarships</h1>
+          <p className="text-sm text-gray-500 mt-1">
+            Browse the list of active scholarships. Use the available actions to
+            modify or remove entries.
+          </p>
+          <div className="container mx-auto py-10 space-y-3">
+            <div>
+              <Input placeholder="Search " />
+            </div>
+            <Table>
+              <TableCaption>A list of active scholarships.</TableCaption>
+              <TableHeader>
                 <TableRow>
-                  <TableCell colSpan={headers.length} className="text-center">
-                    <Ring size={40} speed={2} bgOpacity={0} color="yellow" />
-                  </TableCell>
+                  {headers.map((header) => (
+                    <TableHead key={header.label}>{header.label}</TableHead>
+                  ))}
                 </TableRow>
-              ) : query ? (
-                searchData.map((row) => (
-                  <TableRow key={row.scholarshipId}>
-                    <TableCell className="font-medium underline">
-                      <Link
-                        href={`/administrator/home/scholarships/manage/${row.scholarshipId}`}
-                        prefetch={true}
-                      >
+              </TableHeader>
+              <TableBody>
+                {loading ? (
+                  <TableRow>
+                    <TableCell colSpan={headers.length} className="text-center">
+                      <Ring size={40} speed={2} bgOpacity={0} color="yellow" />
+                    </TableCell>
+                  </TableRow>
+                ) : (
+                  data.map((row, index) => (
+                    <TableRow key={row.scholarshipId}>
+                      <TableCell className="font-medium underline">
+                        <Link
+                          href={`/administrator/home/manage/${row.scholarshipId}`}
+                          prefetch={true}
+                          onClick={() => setIntercept("details")}
+                        >
+                          {index + 1}. {row.scholarshipTitle}
+                        </Link>
+                      </TableCell>
+                      <TableCell className="">
+                        {row.scholarshipProvider}
+                      </TableCell>
+                      <TableCell className="">
+                        <Badge className="bg-green-900 text-gray-300">
+                          Active
+                        </Badge>
+                      </TableCell>
+                      <TableCell className="">
                         {" "}
-                        {row.scholarshipTitle}
-                      </Link>
-                    </TableCell>
-                    <TableCell className="">
-                      {row.scholarshipProvider}
-                    </TableCell>
-                    <TableCell className="">
-                      <Badge className="bg-green-900 text-gray-300">
-                        Active
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="">{row.scholarshipDealine}</TableCell>
-                  </TableRow>
-                ))
-              ) : (
-                data.map((row, index) => (
-                  <TableRow key={row.scholarshipId}>
-                    <TableCell className="font-medium underline">
-                      <Link
-                        href={`/administrator/home/scholarships/manage/${row.scholarshipId}`}
-                        prefetch={true}
-                      >
-                        {index + 1}. {row.scholarshipTitle}
-                      </Link>
-                    </TableCell>
-                    <TableCell className="">
-                      {row.scholarshipProvider}
-                    </TableCell>
-                    <TableCell className="">
-                      <Badge className="bg-green-900 text-gray-300">
-                        Active
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="">
-                      {" "}
-                      {new Date(row.scholarshipDealine).toLocaleDateString()}
-                    </TableCell>
-                  </TableRow>
-                ))
-              )}
-            </TableBody>
-          </Table>
+                        {new Date(row.scholarshipDealine).toLocaleDateString()}
+                      </TableCell>
+                    </TableRow>
+                  ))
+                )}
+              </TableBody>
+            </Table>
+          </div>
         </div>
-      </div>
+      )}
+      {intercept === "details" && <>meow</>}
     </div>
   );
 }
--- src/lib/scholarship-data.ts
+++ src/lib/scholarship-data.ts
@@ -29,7 +29,7 @@ export default function useScholarshipData() {
       } catch (error) {
         console.error(error);
       } finally {
-        setLoading(true);
+        setLoading(false);
       }
     }
 
