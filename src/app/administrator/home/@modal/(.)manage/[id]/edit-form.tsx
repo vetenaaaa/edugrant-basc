5a7b36814dcc9fdfb6a73ea9142d71c75264bc78
iamDaedalus <jerometecson.basc@gmail.com>
2025-07-22 07:38:24 +0800

    .

--- src/app/administrator/home/@modal/(.)manage/[id]/edit-form.tsx
+++ src/app/administrator/home/@modal/(.)manage/[id]/edit-form.tsx
@@ -16,7 +16,7 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Plus, X } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { ScholarshipTypes } from "@/lib/scholarship-data";
 
 const options: Option[] = [
