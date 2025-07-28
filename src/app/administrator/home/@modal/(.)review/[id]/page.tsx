51427e940ff3e5bd7d595b89e2d62d281d2c8d4c
iamDaedalus <jerometecson.basc@gmail.com>
2025-07-28 22:55:01 +0800

    .

--- src/app/administrator/home/@modal/(.)review/[id]/page.tsx
+++ src/app/administrator/home/@modal/(.)review/[id]/page.tsx
@@ -12,6 +12,7 @@ import {
 import { useParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { Badge } from "@/components/ui/badge";
 
 export default function InterceptReviewApplicants() {
   const router = useRouter();
@@ -35,11 +36,24 @@ export default function InterceptReviewApplicants() {
         HandleCloseDrawer(value);
       }}
     >
-      <DrawerContent className="w-[900px] mx-auto h-[95vh] outline-0 border-0">
+      <DrawerContent className="max-w-[900px] w-full mx-auto h-[95vh] outline-0 border-0">
         <DrawerHeader>
-          <DrawerTitle className="text-2xl">Edit Mode</DrawerTitle>
-          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          <DrawerTitle className="text-2xl">Jerome Laguyo Tecson</DrawerTitle>
+          <DrawerDescription>2022000493</DrawerDescription>
+          <div className="flex gap-2">
+            <Badge>BSIT</Badge>
+            <Badge>4th Year</Badge>
+            <Badge>Section C</Badge>
+          </div>
         </DrawerHeader>
+        <div className="overflow-auto">
+          <div className="grid grid-cols-2 p-4 gap-4">
+            <div className="aspect-[16/9] border"></div>
+            <div className="aspect-[16/9] border"></div>
+            <div className="aspect-[16/9] border"></div>
+            <div className="aspect-[16/9] border"></div>
+          </div>
+        </div>
       </DrawerContent>
     </Drawer>
   );
---
+++ src/lib/application-search.ts
@@ -0,0 +1,40 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ApplicationTypes } from "./types";
+
+export default function useApplicationpSearch({ query }: { query: string }) {
+  const [searchData, setSearchData] = useState<ApplicationTypes[]>([]);
+  const [searchLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const trimmedQuery = query.trim();
+    if (!trimmedQuery) {
+      setSearchData([]);
+      setLoading(false);
+      return;
+    }
+    setLoading(true);
+    const delayDebounce = setTimeout(async () => {
+      try {
+        const res = await axios.get(
+          `https://edugrant-express-server-production.up.railway.app/administrator/searchApplication?search=${trimmedQuery}`,
+
+          { withCredentials: true }
+        );
+
+        if (res.status === 200) {
+          setSearchData(res.data.dataSearch);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }, 500); // Adjust delay time as needed
+
+    return () => clearTimeout(delayDebounce);
+  }, [query]);
+
+  return { searchData, searchLoading };
+}
