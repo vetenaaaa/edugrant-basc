c4cb039528f034f1bc1d8166de77c8996d46ecc1
iamDaedalus <jerometecson.basc@gmail.com>
2025-08-05 09:31:39 +0800

    3443

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -2,10 +2,8 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useRouter } from "next/navigation";
 import { ArrowLeft, LoaderCircleIcon } from "lucide-react";
 import Link from "next/link";
-import { useUserStore } from "@/store/useUserStore";
 import {
   Form,
   FormControl,
